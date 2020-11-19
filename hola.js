887c4e7cdd2e9f6449c61e1a79a31282177bed6d
Lucas2710 <Lucas-sohn@hotmail.com>
2020-11-19 02:07:27 +0100

    ahora si

---
+++ hola.js
@@ -0,0 +1,10 @@
+console.log("Lucas Sohn")
+console.log(2+2)
+const lista =["Donnie Darko", "Supernatural", "Harry Potter", "Doctor Who"] 
+const objeto = {
+    peliculas: ["Donnie Darko", "Harry Potter"],
+    series: ["Supernatural", "Doctor Who"]
+}
+console.log(objeto)
+console.log(objeto.peliculas)
+console.log(objeto.series)
